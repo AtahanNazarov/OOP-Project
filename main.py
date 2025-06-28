1e25e1f516c2a7d5ff77b9289cfe68e8b13c431a
Atahan <atahanbuffalo@gmail.com>
2025-06-28 19:55:04 +0200

    Refactor Person class to include get_info method and update usage in main

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 from typing import Literal
 
+# class Prison:
+#     def __init__(self, name: str, capacity: int):
+#         self.name = name
+#         self.capacity = capacity
+        
+#     def add_prisoner(self, prisoner_: str):
+#         self.prisoner_name = prisoner_name
+    
+        
+    # def get_name(self):
+        # return self.name
 class Person:
     def __init__(self, name: str, surname: str, sex: Literal["male", "female"], age: int):
         self.name = name
         self.surname = surname
-        self.age = age
         self.sex = sex
+        self.age = age
         
-    def get_name(self):
-        return self.name
+    def get_info(self):
+        print(
+            f"name: {self.name}\n"
+            f"surname: {self.surname}\n" 
+            f"sex: {self.sex}\n"
+            f"age: {self.age}\n"
+        )
 
 p1 = Person("Edil", "Smith", "male", 12)
 
-print(p1.get_name())
+p1.get_info()
